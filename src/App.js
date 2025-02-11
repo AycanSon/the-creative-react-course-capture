931b71b34d636f40d828413b83b99dc67fb3b50a
AycanSon <aycan110797@googlemail.com>
2025-02-11 11:58:18 +0100

    1. Initial Setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// 1 Neue Packages nutzen für dieses Projekt (z.b. Animationen) mit
+// 1 - npm install react-router-dom framer-motion react-intersection-observer styled-components
+// 1 In Github ein neues repository erstelllen und um einen Link zu erstellen (Wahrscheinlich einen Link für diese Seite)
+// 1 Nach dem Erstellen des Repository diesen Teil git remote add origin https://github.com/AycanSon/the-creative-react-course-capture.git im Terminal eingeben und enter
+// 1 Alle Änderungen pushen (Zu Source Control wechseln, Changes auswählen (alle) und dann schreiben sowas wie 1. Initial Setup )
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Film</h1>
+    </div>
+  );
+}
+
+export default App;
